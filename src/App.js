19221886afe568cfe7065d3e415e71f13770282a
Suriya3621 <suriyaprakashraja849@gmail.com>
2025-01-22 22:03:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { BasicLogin } from "./lib/index.js"
+function App() {
+  return (
+    <div>
+      <BasicLogin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/index.js
@@ -0,0 +1,1 @@
+export { default as BasicLogin } from "./Forms/Login/Login";
